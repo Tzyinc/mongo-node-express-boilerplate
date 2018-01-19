c170c45a2f9cc11738b9e222e164eed189ca44ea
Zhi Yang Ten <tenzy@seagroup.com>
2018-01-19 18:16:32 +0800

    Revert " final commit"
    This reverts commit f4920b4ebb48aac27e0cab1f4a8dabe83bfb24a6.

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
-// packages
-let express = require('express');
-let app = express();
-let bodyParser = require('body-parser');
-let cors = require('cors');
+//packages
+var express 	= require('express');
+var app 		= express();
+var bodyParser 	= require('body-parser');
+var mongoose	= require('mongoose');
 
-app.use(bodyParser.urlencoded({extended: true}));
+//connect to database here. 27017 is the default port for mongo
+mongoose.connect('localhost:27017', (error) => {
+	if (error) {
+		console.error('make sure mongodb is installed and running!');
+		throw error;
+	}
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors());
-// port can be overwritten here
-let port = process.env.PORT || 8888;
 
-// API routes
-let projectRoutes = require('./app/routes/routes.js');
+//port can be overwritten here
+var port = process.env.PORT || 8080;
 
-app.use('/api', projectRoutes);
+//API routes
+var project_routes = require('./app/routes/routes.js');
+
+app.use('/api',project_routes);
 
 app.listen(port);
 console.log('Server started, listening on port: ' + port);
+
